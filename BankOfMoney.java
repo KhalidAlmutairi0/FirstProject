fc1443860de089543b8519cad5cd4a34719b53f5
KhalidAlmutairi0 <445101584@student.ksu.edu.sa>
2024-10-16 20:27:38 +0300

    Update BankOfMoney.java

--- BankOfMoney.java
+++ BankOfMoney.java
@@ -4,16 +4,20 @@ public class BankOfMoney {
 	public static void main(String[] args) {
 
 		String account1Name = "Khalid";
-		double account1Balance = 180000;
+		double account1Balance = 10000.0;
 		String account2Name = "Anas";
-		double account2Balance = 130000.00;
+		double account2Balance = 10000.0;
 		String account3Name = "Abdulaziz";
-		double account3Balance = 150000.00;
+		double account3Balance = 10000.0;
 		double bankProfit = 0.0;
+		double TransferMoney = 0.0;
+		double Charges = 0.0;
+		int Transfers = 0, deposits = 0, withdrawals = 0;
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
-	
+
 			System.out.println("*******************************");
+			System.out.println("Welcome to our bank");
 			System.out.println("Please select an option:");
 			System.out.println("1. Khalid (Account 101)");
 			System.out.println("2. Anas (Account 102)");
@@ -30,19 +34,44 @@ public class BankOfMoney {
 
 			if (choice == 4) {
 				System.out.println("\nAdministrator Menu:");
-				System.out.printf("Total Profit from spending: $%.2f%n", bankProfit);
-				System.out.println("Press any key to return to the main menu.");
-				scanner.next();
+				System.out.printf("Total Profit from funds and Charges: $%.2f%n", bankProfit);
+
+				if (deposits == withdrawals & deposits == Transfers & Transfers == withdrawals) {
+					System.out.println(" all operations are equal");
+				} else if (deposits >= withdrawals & deposits >= Transfers) {
+					if (deposits == withdrawals)
+						System.out.println("withdrawals and deposits are most used operations");
+					else if (deposits == Transfers)
+						System.out.println("Transfers and deposits are most used operations");
+					else
+						System.out.println("deposits is most used operation");
+				} else if (withdrawals >= deposits & withdrawals >= Transfers) {
+					if (withdrawals == deposits)
+						System.out.println("withdrawals and deposits are most used operations");
+					else if (withdrawals == Transfers)
+						System.out.println("withdrawals and Transfers are most used operations");
+					else
+						System.out.println("withdrawals is most used operation");
+				} else {
+					if (Transfers == deposits)
+						System.out.println(" deposits and Transfers are most used operations");
+					else if (Transfers == withdrawals)
+						System.out.println("withdrawals and Transfers are most used operations");
+					else
+						System.out.println("Transfers is most used operation");
+				}
+
+				continue;
 			}
 			String accountName;
 			double accountBalance = 0;
-			if (choice == 1 ||choice == 101 ) {
+			if (choice == 1 || choice == 101) {
 				accountName = account1Name;
 				accountBalance = account1Balance;
-			} else if (choice == 2|| choice == 102 ) {
+			} else if (choice == 2 || choice == 102) {
 				accountName = account2Name;
 				accountBalance = account2Balance;
-			} else if (choice == 3|| choice == 103 ) {
+			} else if (choice == 3 || choice == 103) {
 				accountName = account3Name;
 				accountBalance = account3Balance;
 			} else {
@@ -50,13 +79,13 @@ public class BankOfMoney {
 				continue;
 			}
 			double totalDeposited = 0, totalWithdrawn = 0;
-			int deposits = 0, withdrawals = 0;
 			while (true) {
 				System.out.println("\nAccount Operations for " + accountName);
 				System.out.println("1. View Balance");
 				System.out.println("2. Deposit Money");
 				System.out.println("3. Withdraw Money");
-				System.out.println("4. Exit");
+				System.out.println("4. Transfer Money");
+				System.out.println("5. Exit");
 				int option = scanner.nextInt();
 
 				if (option == 1) {
@@ -64,6 +93,7 @@ public class BankOfMoney {
 				} else if (option == 2) {
 					System.out.print("Enter deposit amount: ");
 					double depositAmount = scanner.nextDouble();
+
 					if (depositAmount > 0) {
 						accountBalance += depositAmount;
 						deposits++;
@@ -86,9 +116,52 @@ public class BankOfMoney {
 						System.out.println("Insufficient funds or invalid amount.");
 					}
 				} else if (option == 4) {
+					System.out.println("Enter The Account that you want to transfer to :");
+					System.out.println("choose from:");
+					System.out.println("1. Khalid (Account 101), 2. Anas (Account 102), 3. Abdulaziz (Account 103).");
+					int SelectedAccount = scanner.nextInt();
+					if (SelectedAccount == choice) {
+						System.out.println("You can't transfer to the same account");
+						continue;
+					} else if (SelectedAccount != 1 && SelectedAccount != 2 && SelectedAccount != 3)
+						System.out.println("incorrect Account");
+					else {
+						System.out.println("Choose The Amount of money to transfer:(2.5% Charges will apply) ");
+						TransferMoney = scanner.nextDouble();
+						Charges = TransferMoney * 0.025;
+
+						if (TransferMoney > 0 && accountBalance >= (TransferMoney + Charges)) {
+							accountBalance -= (TransferMoney + Charges);
+							bankProfit += Charges;
+							Transfers++;
+
+							if (SelectedAccount == 1 || SelectedAccount == 101) {
+								account1Balance += TransferMoney;
+								System.out
+										.println("You have successfully completed a money transfer to " + account1Name);
+								System.out.println("And His New Balance is = " + account1Balance);
+							} else if (SelectedAccount == 2 || SelectedAccount == 102) {
+								account2Balance += TransferMoney;
+								System.out
+										.println("You have successfully completed a money transfer to " + account2Name);
+								System.out.println("And His New Balance is = " + account2Balance);
+							} else if (SelectedAccount == 3 || SelectedAccount == 103) {
+								account3Balance += TransferMoney;
+								System.out
+										.println("You have successfully completed a money transfer to " + account3Name);
+								System.out.println("And His New Balance is = " + account3Balance);
+							}
+						} else {
+							System.out.println("You don't have enough funds.");
+						}
+					}
+				}
+
+				else if (option == 5) {
 					System.out.println("Transaction Summary:");
 					System.out.printf("Total Deposited: $%.2f%n", totalDeposited);
 					System.out.printf("Total Withdrawn: $%.2f%n", totalWithdrawn);
+					System.out.printf("Total Transfer: $%.2f%n", TransferMoney);
 					break;
 				} else {
 					System.out.println("Invalid option. Please try again.");
@@ -105,4 +178,6 @@ public class BankOfMoney {
 		}
 		scanner.close();
 	}
-}
+
+	
+	
